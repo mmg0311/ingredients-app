97fac85fb3acbdf4b96c1de94ba5bb2ab323fd8c
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2019-12-26 12:56:29 +0530

    Added: Sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactSVG } from 'react-svg';
+
+const Sidebar = () => {
+
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    return (
+        <>
+        <Icon onClick={ () => setIsOpen(true) }>
+            <i className="fas fa-bars"/>
+            {/* <ReactSVG src='../assets/icons/bars.svg'/> */}
+        </Icon>
+        <Content open={ isOpen }>
+            <span className="close-icon" onClick={ () => setIsOpen(false) }>
+                <i className="fas fa-times"></i>
+            </span>
+            <ul className="nav-list-top">
+               <li> Ingredients </li>
+               <li> Stations </li> 
+            </ul>
+            <ul className="nav-list-bottom">
+                <li> <i className="fas fa-cog"></i> Manage Settings </li>
+            </ul>
+        </Content>
+        </>
+    );
+}
+
+export default Sidebar;
+
+const Icon = styled.span`
+    padding: 5px 8px;
+    background: #D9E9F1;
+    color: #555B6E;
+    font-size: 2rem;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
+
+const Content = styled.div(
+    ({ open }) => `
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: ${ open ? '0' : '-500px' };
+        color: #555B6E;
+        background: #D9E9F1;
+        min-width: 200px;
+        transition: .5s ease;
+
+        .close-icon {
+            position: absolute;
+            top: 15px;
+            left: 15px;
+            
+            &:hover {
+                cursor: pointer;
+            }
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 50px 0;
+            padding: 15px;
+            font-weight: 500;
+
+            li {
+                border-bottom: 1px solid #D9E9F1;
+                padding: 5px;
+            }
+
+            li:hover {
+                cursor: pointer;
+                border-bottom: 1px solid #fff;
+            }
+
+            li:not(:last-child) {
+                margin-bottom: 15px;
+            }
+        }
+
+        .nav-list-bottom {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+
+            i {
+                margin-right: 12px;
+            }
+        }
+    `
+);
